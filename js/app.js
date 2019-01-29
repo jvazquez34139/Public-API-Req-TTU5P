c64e7a91dbf6d0035f5ee619f988129db06f348d
jose v <jvazquez34139@gmail.com>
2019-01-29 03:26:59 -0600

    makeCards for gallery v1

--- js/app.js
+++ js/app.js
@@ -8,6 +8,30 @@ $.ajax({
 /////////////////////////////////////////
     let employees = data.results;
 
+    //doms
+    const $gallery = $('#gallery');
 
+    //Gallery
+    // data.results.forEach(result => console.log(result.name.first));
+    makeCards(employees,$gallery);
   }
 });//$.ajax
+
+const makeCards = (employees,div) =>{
+  employees.forEach(employee => {
+    const name = employee.name.first + " " + employee.name.last;
+    const email = employee.email;
+    const location = employee.location.city + ", " + employee.location.state;
+    const img = employee.picture.medium;
+    div.append($(`<div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${img}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${name}</h3>
+            <p class="card-text">${email}</p>
+            <p class="card-text cap">${location}</p>
+        </div>
+    </div>`));
+  });
+}
