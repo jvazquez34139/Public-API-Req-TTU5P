171bd3b5c3d53a976803e1770cb02786725bcc16
jose v <jvazquez34139@gmail.com>
2019-02-01 01:27:47 -0600

    added search, next, prev doms

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 //employeesData{employees[12objects],filled:true}
 //location,name,picture,email,phone,dob
-//doms
+//DOMS////////////////////////////////////
 const $card = $('.card');
 const $modalContainer = $('<div class="modal-container"></div>');
 const $modal = $('<div class="modal">');
 const $close = $('<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>');
 const $modalInfo = $('<div class="modal-info-container">');
+const $modalbtns = $('<div class="modal-btn-container"></div>');
+const $modalprev = $('<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>');
+const $modalNext = $('<button type="button" id="modal-next" class="modal-next btn">Next</button>');
+const $search = $('.search-container');
+const $searchForm = $('<form action="#" method="get"></form>');
+const $searchInput = $('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
+const $submit = $('<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">');
 //the info doms
 const $img = $('<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">');
 const $name = $('<h3 id="name" class="modal-name cap">name</h3>');
@@ -16,22 +23,29 @@ const $phone = $('<p class="modal-text">(555) 555-5555</p>');
 const $location = $('<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>');
 const $dob = $('<p class="modal-text">Birthday: 10/21/2015</p>');
 //append to appreate dom
+const infoDoms = [$img,$name,$email,$city,$hr,$phone,$location,$dob];
 $modalContainer.insertAfter($('#gallery'));
 $modalContainer.append($modal);
 $modal.append($close);
 $modal.append($modalInfo);
-$modalInfo.append($img);
-$modalInfo.append($name);
-$modalInfo.append($email);
-$modalInfo.append($city);
-$modalInfo.append($hr);
-$modalInfo.append($phone);
-$modalInfo.append($location);
-$modalInfo.append($dob);
+infoDoms.forEach(dom => $modalInfo.append(dom));
+$modalContainer.append($modalbtns);
+$modalbtns.append($modalprev);
+$modalbtns.append($modalNext);
+$search.append($searchForm);
+$searchForm.append($searchInput);
+$searchForm.append($submit);
 //hide modal by default
 $modalContainer.hide();
 
-// event listeners
+//GLOBAL VARIABLES////////////////////////
+//holds which imployee info displayed
+//when switching with prev and next
+let empInView = {};
+//searched list to flip through
+let searchedEmps = employeesData.employees;
+
+//EVENT LISTENERS/////////////////////////
 $card.on('click',function(e){
   let employee =
   employeesData.employees.filter(employee => {
@@ -39,12 +53,24 @@ $card.on('click',function(e){
       return employee;
     }
   })[0];
+  empInView = employee;
   makeModal(employee);
+});
+$submit.on('click',function(e){
+  e.preventDefault();
+  
+})
+$modalprev.on('click',function(){
+
+});
+$modalNext.on('click',function(){
+
 });
 $close.on('click',function(){
   $modalContainer.hide();
 });
-//displays modal with udpdated info
+
+//HELPER FUNCTIONS////////////////////////
 const makeModal = employee => {
   $img.attr('src',employee.picture.medium);
   $name.text(employee.name.first + " " + employee.name.last);
